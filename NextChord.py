b211fef8be99c2bdeee9e30bf785d5460449470f
Stanislaw Robak <srobak@architech.ca>
2017-05-15 21:08:09 +0200

    add readme.md and bootstrap form

--- NextChord.py
+++ NextChord.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, flash
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+from flask_wtf import Form, RecaptchaField
+from flask_wtf.file import FileField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, validators
+from wtforms.validators import Required
 
-app = Flask(__name__)
+
+class ExampleForm(Form):
+    model = TextField('Model Field', description='zamienic na dropdown z wyborem gotowego wytrenowanego modelu z melody rnn')
+    workspace = TextField('Workspace Field', description='wybór workspace',
+                       validators=[Required()])
+    checkbox_field = BooleanField('jakies kontorlki od podstaowwej konfiguracji',
+                                  description='Checkbox description')
+
+    submit_button = SubmitField('Generuj')
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
 
+
+def create_app(configfile=None):
+    app = Flask(__name__)
+    AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
+                                # highly recommend =)
+                                # https://github.com/mbr/flask-appconfig
+    Bootstrap(app)
+
+    # in a real app, these should be configured through Flask-Appconfig
+    app.config['SECRET_KEY'] = 'devkey'
+    app.config['RECAPTCHA_PUBLIC_KEY'] = \
+        '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+    @app.route('/', methods=('GET', 'POST'))
+    def index():
+        form = ExampleForm()
+        form.validate_on_submit()  # to get error messages to the browser
+        return render_template('index.html', form=form)
+
+    return app
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run(debug=True)
---
+++ README.md
@@ -0,0 +1,11 @@
+## Install dependecies
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+```
+export FLASK_APP=NextChord.py
+flask run
+```
